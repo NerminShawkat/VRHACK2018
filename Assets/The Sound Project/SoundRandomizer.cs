4a8d15cb31424529d27fa755fa4f3e1d84b1d630
NerminShawkat <shawkat.nermin@gmail.com>
2018-11-19 01:30:42 +0200

    finally

---
+++ Assets/The Sound Project/SoundRandomizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SoundRandomizer : MonoBehaviour {
+    [SerializeField]
+    AudioClip[] clips;
+    [SerializeField]
+    Range _volume;
+    [SerializeField]
+    Range _time;
+
+    IEnumerator Start()
+    {
+        while(true)
+        {
+            SFXManager.Instance.PlayClip(clips[Random.Range(0, clips.Length)], _volume.Value);
+            yield return new WaitForSeconds(_time.Value);
+        }
+    }
+
+}
+[System.Serializable]
+public class Range
+{
+    [SerializeField]
+    float _min;
+    [SerializeField]
+    float _max;
+
+    public float Value
+    {
+        get
+        {
+            return Random.Range(_min, _max);
+        }
+    }
+}
